d79464b6e4fa3fa1c044e4f9c99e798a366d458b
fukudck <75591795+fukudck@users.noreply.github.com>
2025-06-20 01:46:03 +0700

    Enhance file upload API to include password hashing, expiration options, and metadata handling

--- app/api/upload/route.ts
+++ app/api/upload/route.ts
@@ -5,12 +5,10 @@ import { NextRequest, NextResponse } from 'next/server';
 import fs from 'fs';
 import path from 'path';
 import { writeFile } from 'fs/promises';
-
+import bcrypt from 'bcrypt';
 import { v4 as uuidv4 } from 'uuid';
 
-
 const prisma = new PrismaClient();
-
 
 export async function POST(req: NextRequest) {
   const session = await auth();
@@ -21,21 +19,64 @@ export async function POST(req: NextRequest) {
   }
 
   try {
-    console.log("User UUID: ", session.user?.id)
-
-    const files = data.getAll('file') as File[];
-    if (files.length === 0) {
-      return NextResponse.json({ error: 'No file uploaded' }, { status: 400 });
+    //Get password
+    const password = data.get('password')?.toString();
+    let passwordHash: string | null = null;
+    if (password && password.length > 0) {
+      const saltRounds = 10;
+      passwordHash = await bcrypt.hash(password, saltRounds);
     }
 
-    const savedFiles = [];
+    // Get ExpiredOption
+    const expiredOption = data.get('expiredOption')?.toString()?.toLowerCase();
+    let expiredAt: Date | null = null;
 
+    if (!expiredOption) {
+      return NextResponse.json({ error: 'Missing expiration option' }, { status: 400 });
+    }
+
+    if (expiredOption === 'never') {
+      expiredAt = null;
+    } else {
+      const days = parseInt(expiredOption);
+      if (isNaN(days) || ![1, 3, 7, 14, 30].includes(days)) {
+        return NextResponse.json({ error: 'Invalid expiration option' }, { status: 400 });
+      }
+      expiredAt = new Date(Date.now() + days * 24 * 60 * 60 * 1000);
+    }
+
+    // Get Title, message
+    let title = data.get('title')?.toString()?.trim();
+    if (!title || title.length === 0) {
+      title = 'Unnamed';
+    }
+
+    const message = data.get('message')?.toString()?.trim() || null;
+
+    
+
+    // Create dir
     const folderUUID = uuidv4();
-
-    console.log("Upload UUID: ", folderUUID)
+    const shareLink = await prisma.shareLink.create({
+      data: {
+        token: folderUUID,
+        expiredAt: expiredAt ?? undefined,
+        passwordHash,
+        creator: { connect: { id: session.user!.id } },
+        title: title,        // luôn có (nếu thiếu thì là 'Unnamed')
+        message : message       // optional
+      }
+    });
     const uploadDir = path.join(process.cwd(), 'uploads', folderUUID);
     if (!fs.existsSync(uploadDir)) {
       fs.mkdirSync(uploadDir, { recursive: true });
+    }
+
+    // Get Files
+    const savedFiles = [];
+    const files = data.getAll('file') as File[];
+    if (files.length === 0) {
+      return NextResponse.json({ error: 'No file uploaded' }, { status: 400 });
     }
     for (const file of files) {
       const bytes = await file.arrayBuffer();
@@ -48,19 +89,25 @@ export async function POST(req: NextRequest) {
       const url = `/uploads/${folderUUID}/${file.name}`;
       savedFiles.push(url);
     
-      // await prisma.file.create({
-      //   data: {
-      //     filename: file.name ?? 'unknown',
-      //     size: file.size,
-      //     mime: file.type ?? 'application/octet-stream',
-      //     url: url,
-      //     ownerId: session.user!.id, // dùng ! để nói với TS: "id chắc chắn tồn tại"
-      //   },
-      // });      
+      await prisma.file.create({
+        data: {
+          filename: file.name ?? 'unknown',
+          size: file.size,
+          mime: file.type ?? 'application/octet-stream',
+          url: url,
+          ownerId: session.user!.id, 
+          shareLinkId: shareLink.id
+        },
+      });      
     }
 
 
-    return NextResponse.json({ message: 'Upload thành công', uploadedBy: session.user?.id });
+    return NextResponse.json({
+      message: 'Upload thành công',
+      uploadedBy: session.user?.id,
+      downloadLink: `/download/${shareLink.token}`,
+      expiredAt
+    });
   } catch (error) {
     console.error('Upload error:', error);
     return NextResponse.json({ error: 'Upload failed' }, { status: 500 });
